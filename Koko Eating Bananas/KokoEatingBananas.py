853daf5e28308222dbbd5c4214aac7448f2f914b
Eden <xiang_xuan@qq.com>
2022-05-23 19:03:27 +0800

    :sparkles: feat: Koko Eating Bananas

---
+++ Koko Eating Bananas/KokoEatingBananas.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :param piles: List[int]
+        :param h: int
+        :return: int
+        """
+        pass
+
+if __name__ == '__main__':
+    piles = [3, 6, 7, 11]
+    h = 8
+    solution = Solution()
+    result = solution.minEatingSpeed(piles, h)
+    print(result)
