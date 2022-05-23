3db1127af8e142bb0752f1b3f3e12bd7e17ede5d
Eden <xiang_xuan@qq.com>
2022-05-23 19:03:52 +0800

    :sparkles: feat: K Closest element

---
+++ Find K Closest Element/KClosest.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :param arr: List[int]
+        :param k: int
+        :param x: int
+        :return: List[int]
+        """
+        pass
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 3, 4, 5]
+    k = 4
+    x = 3
+
+    solution = Solution()
+    result = solution.findClosestElements(arr, k, x)
+    print(result)
+    
